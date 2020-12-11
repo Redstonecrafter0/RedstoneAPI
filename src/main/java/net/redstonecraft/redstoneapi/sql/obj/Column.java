76b00920a9c61dd5fe247c538f3acea86281e025
Redstonecrafter0 <pfohefernando@gmail.com>
2020-12-11 21:20:20 +0100

    updated

--- src/main/java/net/redstonecraft/redstoneapi/sql/obj/Column.java
+++ src/main/java/net/redstonecraft/redstoneapi/sql/obj/Column.java
@@ -4,10 +4,12 @@ public class Column {
 
     public final String name;
     public final Object value;
+    public final Class<? extends SQLVariable> type;
 
     public Column(String name, Object value, Class<? extends SQLVariable> type) {
         this.name = name;
         this.value = value;
+        this.type = type;
     }
 
 }
