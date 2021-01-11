dbb6f440870e4f726382a87e10b05a86b2e62f31
Redstonecrafter0 <54239558+Redstonecrafter0@users.noreply.github.com>
2021-01-11 19:58:30 +0100

    updated

---
+++ src/main/java/net/redstonecraft/redstoneapi/tools/HttpHeader.java
@@ -0,0 +1,13 @@
+package net.redstonecraft.redstoneapi.tools;
+
+public class HttpHeader {
+
+    public final String key;
+    public final String value;
+
+    public HttpHeader(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/net/redstonecraft/redstoneapi/tools/HttpRequest.java
@@ -0,0 +1,51 @@
+package net.redstonecraft.redstoneapi.tools;
+
+import sun.misc.IOUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpRequest {
+
+    public final byte[] content;
+    public final int responseCode;
+    public final String mimeType;
+
+    public HttpRequest(byte[] content, int responseCode, String mimeType) {
+        this.content = content;
+        this.responseCode = responseCode;
+        this.mimeType = mimeType;
+    }
+
+    public static HttpRequest get(String url, HttpHeader... header) throws IOException {
+        URL urlUrl = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlUrl.openConnection();
+        for (HttpHeader i : header) {
+            con.addRequestProperty(i.key, i.value);
+        }
+        byte[] response = IOUtils.readAllBytes(con.getInputStream());
+        int code = con.getResponseCode();
+        String mime = con.getContentType();
+        con.disconnect();
+        return new HttpRequest(response, code, mime);
+    }
+
+    public static HttpRequest post(String url, byte[] content, HttpHeader... header) throws IOException {
+        URL urlUrl = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) urlUrl.openConnection();
+        for (HttpHeader i : header) {
+            con.addRequestProperty(i.key, i.value);
+        }
+        OutputStream out = con.getOutputStream();
+        out.write(content);
+        out.flush();
+        byte[] response = IOUtils.readAllBytes(con.getInputStream());
+        int code = con.getResponseCode();
+        String mime = con.getContentType();
+        con.disconnect();
+        return new HttpRequest(response, code, mime);
+    }
+
+}
